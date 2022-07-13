71aef5e924c5008c6d3aa459a3a5b87a4fb58358
begalievn <begaliev.info@gmail.com>
2022-07-13 17:00:24 +0600

    initialized a project

---
+++ src/components/testComponent/TestComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './testComponent.module.scss';
+
+const TestComponent = () => {
+  return <div className={classes.test}>TestComponent</div>;
+};
+
+export default TestComponent;
