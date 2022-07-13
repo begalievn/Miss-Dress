887275bc9a1dadd5530af1ee77faaafb86080dc8
begalievn <begaliev.info@gmail.com>
2022-07-13 17:18:26 +0600

    Changed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import TestComponent from './components/testComponent/TestComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <TestComponent />
     </div>
   );
 }
