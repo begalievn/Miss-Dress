fbffcd74c0e2ffd8e3e1cc7766bf71ca0c3e48ff
begalievn <begaliev.info@gmail.com>
2022-07-13 17:03:26 +0600

    Deleted TestComponent folder

--- src/components/testComponent/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './testComponent.module.scss';
-
-const TestComponent = () => {
-  return <div className={classes.test}>TestComponent</div>;
-};
-
-export default TestComponent;
