64d593d9499b21a0c5600ce34ef3ba19eb596d8f
Talgat <batyralievtalgat98@gmail.com>
2022-07-13 17:27:08 +0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import TestComponent from './components/testComponent/TestComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
-      <TestComponent />
+
     </div>
   );
 }
