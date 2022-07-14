a6a74359d87e31c77fcde4c141fbb89eaf389525
Talgat <batyralievtalgat98@gmail.com>
2022-07-14 12:52:18 +0600

    header

--- src/containers/header/Header.tsx
+++ src/containers/header/Header.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import hero from '../../assets/header/hero.svg'
+import heart from '../../assets/header/heart.svg'
+import search from '../../assets/header/search.svg'
+import shopping from '../../assets/header/shopping-bag.svg'
+import sign from '../../assets/header/sign-in.svg'
+import user from '../../assets/header/user.svg'
 import style from './Header.module.scss'
 const Header = () => {
 
@@ -18,8 +25,10 @@ const Header = () => {
    
     return (
         <div className={style.mainBlock} >
-            <div>MissDress</div>
-           
+            <div className={style.container}>
+            <Link to="/">
+            <div className={style.logo} >MissDress <img src={hero} alt="hero" /></div>
+            </Link>
                 <ul className={style.mainBlock_navbar}>
                     {pages.map(page =>(
                         <Link to={page.link}>
@@ -27,7 +36,14 @@ const Header = () => {
                      </Link>
                     ))}
                 </ul> 
-            <div>Иконки</div>
+            <div className={style.icons} > 
+                <img src={search} alt="" />
+                <img src={heart} alt="" />
+                <img src={shopping} alt="" />
+                <img src={sign} alt="" />
+                {/* <img src={user} alt="" /> */}
+            </div>
+            </div>
         </div>
     );
 };
