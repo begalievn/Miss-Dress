e2dd48be2434206e68c7a42387e69648097fe209
Давид <brayzgunov28@gmail.com>
2022-07-18 11:15:45 +0600

    created new branch

--- src/pages/mainPage/collection/Collection.tsx
+++ src/pages/mainPage/collection/Collection.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 
-import styles from "./collection.module.scss"
+import styles from "./collection.module.scss";
+import Block from "./components/block/Block";
 
 function Collection() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.header}>
-        Коллекция
-      </h1>
-      <div className={styles.test}>
-        Коллекция
-      </div>
+      <h1 className={styles.header}>Коллекция</h1>
+      <div className={styles.test}>Коллекция</div>
+      {[1, 2, 3, 4].map((item) => {
+        return <Block />;
+      })}
     </div>
-    
   );
 }
 
---
+++ src/pages/mainPage/collection/components/block/Block.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./block.module.scss";
+
+function Block() {
+  return <div className={styles.block}>asda</div>;
+}
+
+export default Block;
