16a4070ad774c6f8e16caf41bb36480473171c15
begalievn <begaliev.info@gmail.com>
2022-07-20 11:07:48 +0600

    MD2-16 Added cateogories sidebar

--- src/pages/productsPage/components/categoriesAside/CategoriesAside.tsx
+++ src/pages/productsPage/components/categoriesAside/CategoriesAside.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import classes from "./categoriesAside.module.scss";
 
-const data = [
+const categories = [
   {
     id: 1,
     title: "Верхняя одежда",
@@ -57,7 +57,104 @@ const data = [
 ];
 
 const CategoriesAside = () => {
-  const [isVisibleNested, setVisibleNested] = useState(false);
+  const [activeMenus, setActiveMenus] = useState<string[]>([]);
+
+  const handleMenuClick = (data: any) => {
+    console.log(data);
+  };
+
+  const handleArrowClick = (menuName: string) => {
+    let newActiveMenus = [...activeMenus];
+
+    if (newActiveMenus.includes(menuName)) {
+      let index = newActiveMenus.indexOf(menuName);
+      if (index > -1) {
+        newActiveMenus.splice(index, 1);
+      }
+    } else {
+      newActiveMenus.push(menuName);
+    }
+
+    setActiveMenus(newActiveMenus);
+  };
+
+  if (categories[2].children) {
+    console.log("has children", categories[2].children);
+  }
+
+  interface IListMenu {
+    dept: any;
+    data: any;
+    hasChildren: any;
+    menuName: string;
+    menuIndex: any;
+  }
+
+  const ListMenu = ({
+    dept,
+    data,
+    hasChildren,
+    menuName,
+    menuIndex,
+  }: IListMenu) => (
+    <li>
+      <div className={classes.item} style={{ paddingLeft: `${dept * 18}px` }}>
+        <span className={classes.label} onClick={() => handleMenuClick(data)}>
+          {data.title}
+        </span>
+        {hasChildren.length !== 0 && (
+          <span
+            onClick={() => handleArrowClick(menuName)}
+            className={classes.arrow}
+            style={{
+              transform: activeMenus.includes(menuName)
+                ? "rotate(180deg)"
+                : "rotate(0deg)",
+            }}
+          ></span>
+        )}
+      </div>
+      {hasChildren && (
+        <SubMenu
+          dept={dept}
+          data={data.children}
+          toggle={activeMenus.includes(menuName)}
+          menuIndex={menuIndex}
+        />
+      )}
+    </li>
+  );
+
+  interface ISubMenu {
+    dept: any;
+    data: Array<any>;
+    toggle: boolean;
+    menuIndex: number;
+  }
+
+  const SubMenu = ({ dept, data, toggle, menuIndex }: ISubMenu) => {
+    if (!toggle) {
+      return null;
+    }
+    dept = dept + 1;
+    return (
+      <ul>
+        {data.map((menu: any, index: number) => {
+          const menuName = `sidebar-submenu-${dept}-${menuIndex}-${index}`;
+          return (
+            <ListMenu
+              dept={dept}
+              data={menu}
+              hasChildren={menu.children}
+              menuName={menuName}
+              key={menuName}
+              menuIndex={index}
+            />
+          );
+        })}
+      </ul>
+    );
+  };
 
   return (
     <div className={classes.container}>
@@ -66,21 +163,18 @@ const CategoriesAside = () => {
       </div>
       <div className={classes.categories}>
         <ul>
-          {data.map((item, index) => {
-            if (item.children.length === 0) {
-              return <li>{item.title}</li>;
-            } else {
-              return (
-                <li>
-                  <li>{item.title}</li>
-                  <ul className={""}>
-                    {item.children.map((childItem, childIndex) => (
-                      <li>{childItem.title}</li>
-                    ))}
-                  </ul>
-                </li>
-              );
-            }
+          {categories.map((menu, index) => {
+            const dept = 1;
+            const menuName = `sidebar-menu-${dept}-${index}`;
+            return (
+              <ListMenu
+                dept={dept}
+                data={menu}
+                hasChildren={menu.children}
+                menuName={menuName}
+                menuIndex={index}
+              />
+            );
           })}
         </ul>
       </div>
