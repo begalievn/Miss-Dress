74fc1576a0808ae6f84ef74385509b4de0b040b6
Talgat <batyralievtalgat98@gmail.com>
2022-07-22 10:43:38 +0600

    md2-30 error-page

---
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { useAppSelector } from "../../utils/app/hooks";
+
+import style from "./Breadcrumbs.module.scss";
+const Breadcrumbs = () => {
+
+  const breadcrumbs = useAppSelector(state=>state.Breadcrumbs.breadcrumbs);
+
+
+
+  
+  return (
+    <div className={style.mainBlock}>
+      <div>Главная</div>
+    <div>/</div>
+      {breadcrumbs.map(item=>(
+        <>
+        <div>{item}</div>
+        <div>/</div>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Breadcrumbs;
