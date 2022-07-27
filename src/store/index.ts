bb24f2f72efa1ce935e2dc19235f4d832e1cb964
Мамед Джафаров <deff2666@gmail.com>
2022-07-27 17:54:46 +0600

    Update

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import AuthorizationUserSlice from "./reducers/AuthorizationUserSlice";
 import Breadcrumbs from "./reducers/BreadcrumbsSlice";
 import ModalSlice from "./reducers/ModalSlice";
-import { CategoryApi } from "./services/CategoryApi";
+import { CategoryApi } from "./services/categoryApi";
 import { LikeApi } from "./services/LikeApi";
 import { categoryApi } from "./services/categoryApi";
 
