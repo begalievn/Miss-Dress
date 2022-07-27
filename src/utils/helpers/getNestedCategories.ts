ce98b2bde7d5f6b8066eeb461eb840f98e9a9f09
Мамед Джафаров <deff2666@gmail.com>
2022-07-27 18:40:29 +0600

    update

--- src/utils/helpers/getNestedCategories.ts
+++ src/utils/helpers/getNestedCategories.ts
@@ -7,19 +7,15 @@ export function getNestedCategories(
 
   function dig(categories: CategoryTypes[]) {
     categories.forEach((item) => {
-      console.log(item);
-
       if (item.children.length > 0) {
-        console.log("Закончить");
-        return item.children;
+        dig(item.children);
       } else {
-        console.log("Продолжить");
-        return result.push(item);
+        result.push(item);
       }
     });
   }
 
   dig(categories);
-  // console.log(result);
+  console.log(result);
   return result;
 }
