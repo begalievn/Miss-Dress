430859b2b7c5e178ea8e870c94759257a17ea5fe
Давид <brayzgunov28@gmail.com>
2022-07-28 18:05:00 +0600

    добавил переход на новости

--- src/pages/mainPage/components/newsList/NewsList.tsx
+++ src/pages/mainPage/components/newsList/NewsList.tsx
@@ -1,48 +1,69 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 
-import { subBackMobile } from "../../../../assets/main-page/images";
+import { useNavigate } from "react-router-dom";
+
+import { IList } from "../../../newsPage/NewsArray";
 
 import styles from "./newList.module.scss";
 
 interface INewsList {
-  page?: "main" | "currentNews" | "about";
-  title?: string;
+  title: "Другие новости" | "Новости";
+  info: IList[] | null | undefined;
 }
 
-const NewsList: FC<INewsList> = ({ page = "main", title = "Новости" }) => {
+const NewsList: FC<INewsList> = ({ title = "Новости", info }) => {
+  const navigate = useNavigate();
+
+  const zero = (int: number) => {
+    if (int < 10) {
+      return `0${int}`;
+    }
+    return int;
+  };
+
+  console.log(
+    `${zero(new Date().getDay())}.${zero(
+      new Date().getMonth()
+    )}.${new Date().getFullYear()}`
+  );
+
   return (
     <section className={styles.container}>
-      {page !== "about" && (
+      {title === undefined && (
         <h1
           className={
-            page === "main" ? styles.headerMain : styles.headerCurrentNews
+            title === "Новости" ? styles.headerMain : styles.headerCurrentNews
           }
         >
           {title}
         </h1>
       )}
-      {[1, 2, 3, 4].map((item) => (
-        <div key={item} className={styles.imageBlock}>
+
+      {info &&
+        info.map((item: IList) => (
           <div
-            style={{
-              backgroundImage: `url(${subBackMobile})`,
-            }}
-            className={
-              page === "about"
-                ? `${styles.infoBlock} ${styles.lost}`
-                : `${styles.infoBlock}`
-            }
+            onClick={() => navigate(`/news/${item.id}`)}
+            key={item.id}
+            className={styles.imageBlock}
           >
-            <h3 className={styles.title}>Более 20 новых коллекций</h3>
-            <div className={styles.info}>
-              <p className={styles.read}>Читать</p>
-              <p className={styles.date}>05.05.2022</p>
+            <div
+              style={{
+                backgroundImage: `url(${item.image})`,
+              }}
+              className={
+                title === undefined
+                  ? `${styles.infoBlock} ${styles.lost}`
+                  : `${styles.infoBlock}`
+              }
+            >
+              <h3 className={styles.title}>{item.title}</h3>
+              <div className={styles.info}>
+                <p className={styles.read}>Читать</p>
+                <p className={styles.date}>{item.date}</p>
+              </div>
             </div>
           </div>
-
-          {/*<img src={newsListImage} alt="" className={styles.image} />*/}
-        </div>
-      ))}
+        ))}
     </section>
   );
 };
--- src/pages/newsPage/NewsPage.tsx
+++ src/pages/newsPage/NewsPage.tsx
@@ -1,34 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useParams } from "react-router-dom";
 
 import NewsList from "../mainPage/components/newsList/NewsList";
 
 import styles from "./newsPage.module.scss";
 
-import image from "./newsImage.jpg";
+import { IList, newsArray } from "./NewsArray";
+
+interface INewsPage {
+  currentNews: IList;
+  otherNews: IList[];
+}
 
 const NewsPage = () => {
+  const [refreshInfo, setRefreshInfo] = useState<INewsPage | null>(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    const other = newsArray.filter((item) => {
+      return item.id.toString() !== params.id;
+    });
+    const one = newsArray.filter((item) => {
+      return item.id.toString() === params.id;
+    })[0];
+
+    setRefreshInfo({ currentNews: one, otherNews: other });
+  }, [params]);
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h3 className={styles.titleMobile}>Она создала свой бренд в 20</h3>
+        <h3 className={styles.titleMobile}>{refreshInfo?.currentNews.title}</h3>
         <section className={styles.newsContainer}>
-          <img src={image} alt="" className={styles.image} />
+          <img
+            src={refreshInfo?.currentNews.image}
+            alt=""
+            className={styles.image}
+          />
           <div className={styles.block}>
             <section className={styles.textBlock}>
-              <h3 className={styles.title}>Она создала свой бренд в 20</h3>
-              <p className={styles.text}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
-                eos excepturi illo nam quidem tempora totam vel. Accusamus,
-                atque blanditiis dolore facere id nam neque nobis odit omnis
-                pariatur perferendis possimus quia reiciendis rerum sunt totam
-                voluptate, voluptates voluptatibus? Ab, blanditiis consequatur
-                dolorum earum fuga itaque minus molestiae nisi saepe tenetur ut
-              </p>
+              <h3 className={styles.title}>{refreshInfo?.currentNews.title}</h3>
+              <p className={styles.text}>{refreshInfo?.currentNews.text}</p>
             </section>
-            <p className={styles.date}>20.01.2022</p>
+            <p className={styles.date}>{refreshInfo?.currentNews.date}</p>
           </div>
         </section>
-        <NewsList page="currentNews" title="Другие новости" />
+        <NewsList title="Другие новости" info={refreshInfo?.otherNews} />
       </div>
     </div>
   );
