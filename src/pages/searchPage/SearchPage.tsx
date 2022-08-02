bbe22e36a41c5b8a12431b8fa3a6ac8ff14beac2
ilimKUSH <ilimKUSH>
2022-08-02 11:30:58 +0600

    search changes

--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -3,52 +3,19 @@ import { useParams } from "react-router-dom";
 
 import { searchApi } from "../../store/services/SearchApi";
 
-import classes from "./search.module.scss";
-
 import Breadcrumbs from "../../components/breadcrumbs/Breadcrumbs";
+
 import CategoriesDropdowBtn from "../../components/categoriesDropdowButton/CategoriesDropdowBtn";
-import ContentSearch from "./components/ContentSearch";
+
 import Paginations from "../../components/pagination/Paginations";
 
-import {
-  bestSellers1,
-  bestSellers2,
-  bestSellers3,
-  bestSellers4,
-  bestSellers5,
-  bestSellers6,
-} from "../../assets/main-page/images";
-import SimilarProductsSlider from "../../components/similar-products-slider/SimilarProductsSlider";
-import SimilarProductsBlock from "../../components/similar-products-block/SimilarProductsBlock";
+import BestSellersBlock from "../mainPage/components/best-sellers-block/BestSellersBlock";
+
+import classes from "./search.module.scss";
+
+import ContentSearch from "./components/ContentSearch";
 
 const SearchPage = () => {
-  const images = [
-    {
-      image: bestSellers1,
-      id: 1,
-    },
-    {
-      image: bestSellers2,
-      id: 2,
-    },
-    {
-      image: bestSellers3,
-      id: 3,
-    },
-    {
-      image: bestSellers4,
-      id: 4,
-    },
-    {
-      image: bestSellers5,
-      id: 5,
-    },
-    {
-      image: bestSellers6,
-      id: 6,
-    },
-  ];
-
   const { name } = useParams();
 
   const { data = [], isLoading, isError } = searchApi.useFetchSearchQuery(name);
@@ -64,7 +31,7 @@ const SearchPage = () => {
             <h2 className={classes.title}>Результаты поиска</h2>
             <CategoriesDropdowBtn />
           </div>
-          <p>Найдено {cards.length} моделей</p>
+          <p className={classes.subtitle}>Найдено {cards.length} моделей</p>
         </div>
         {isLoading && <h1>Идет загрузка...</h1>}
         {isError && <h1>Произошла ошибка при загрузке...</h1>}
@@ -84,7 +51,7 @@ const SearchPage = () => {
           ))
         ) : (
           <div className={classes.content_main_block}>
-            <SimilarProductsBlock />
+            <BestSellersBlock />
           </div>
         )}
       </div>
@@ -94,89 +61,3 @@ const SearchPage = () => {
 };
 
 export default SearchPage;
-
-// import React from "react";
-//
-// import {
-//   bestSellers1,
-//   bestSellers2,
-//   bestSellers3,
-//   bestSellers4,
-//   bestSellers5,
-//   bestSellers6,
-// } from "../../assets/main-page/images";
-//
-// import Breadcrumbs from "../../components/breadcrumbs/Breadcrumbs";
-// import CategoriesDropdowBtn from "../../components/categoriesDropdowButton/CategoriesDropdowBtn";
-// import Paginations from "../../components/pagination/Paginations";
-//
-// import ContentSearch from "./components/ContentSearch";
-//
-// import classes from "./search.module.scss";
-// import { searchApi, useFetchSearchQuery } from "../../store/services/SearchApi";
-// import { useParams } from "react-router-dom";
-// import { data } from "../../components/main-page/photo-card-content/PhotoCardContent";
-//
-// const cards = [
-//   {
-//     image: bestSellers1,
-//     id: 1,
-//   },
-//   {
-//     image: bestSellers2,
-//     id: 2,
-//   },
-//   {
-//     image: bestSellers3,
-//     id: 3,
-//   },
-//   {
-//     image: bestSellers4,
-//     id: 4,
-//   },
-//   {
-//     image: bestSellers5,
-//     id: 5,
-//   },
-//   {
-//     image: bestSellers6,
-//     id: 6,
-//   },
-// ];
-//
-// const SearchPage = () => {
-//   const { name } = useParams();
-//
-//   const {
-//     data: card,
-//     isLoading,
-//     isError,
-//   } = searchApi.useFetchSearchQuery(name);
-//
-//   console.log(data);
-//
-//   return (
-//     <>
-//       <div className={classes.content_main}>
-//         <div className={classes.content_parent}>
-//           <Breadcrumbs />
-//           <div className={classes.result}>
-//             <h2 className={classes.title}>Результаты поиска</h2>
-//             <CategoriesDropdowBtn />
-//           </div>
-//           <p>Найдено {cards.length} моделей</p>
-//         </div>
-//         {isLoading && <h1>Идет загрузка...</h1>}
-//         {isError && <h1>Произошла ошибка при загрузке...</h1>}
-//         {cards.map((item, id) => (
-//           <div>
-//             <ContentSearch key={id} image={item.image} id={item.id} />
-//           </div>
-//         ))}
-//       </div>
-//       <Paginations />
-//     </>
-//   );
-// };
-//
-// export default SearchPage;
--- src/pages/searchPage/components/ContentSearch.tsx
+++ src/pages/searchPage/components/ContentSearch.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import classes from "../search.module.scss";
 
 import ProductPhoto from "../../../components/productPhoto/ProductPhoto";
@@ -10,18 +12,9 @@ import {
   favoriteIcon,
 } from "../../../assets/icons/icons";
 
-import { useNavigate } from "react-router-dom";
-
 import { ProductTest } from "../../../utils/types/types";
 
-const ContentSearch = ({
-  id,
-  title,
-  price,
-  discount,
-  rate,
-  image,
-}: ProductTest) => {
+const ContentSearch = ({ id, title, price, discount, rate }: ProductTest) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -31,9 +24,6 @@ const ContentSearch = ({
   return (
     <>
       <div className={classes.content}>
-        {/*{data.map((item) => (*/}
-        {/*  <div>{item.name}</div>*/}
-        {/*))}*/}
         <div className={classes.container}>
           <div>
             <ProductPhoto
@@ -44,12 +34,12 @@ const ContentSearch = ({
           <div onClick={handleClick} className={classes.content}>
             <div className={classes.first}>
               <div className={classes.price}>
-                <span className={classes.old_price}>{discount}</span>
+                <span className={classes.old_price}>200</span>
                 <span className={classes.new_price}>{price}</span>
               </div>
               <div className={classes.colors_container}>
                 <img src={colorsPalletIcon} alt={"color pallet icon"} />
-                <span className={classes.colors}>colors</span>
+                <span className={classes.colors}>4</span>
               </div>
             </div>
             <div className={classes.second}>
@@ -57,7 +47,7 @@ const ContentSearch = ({
             </div>
             <div className={classes.third}>
               <p>
-                Размер <span>size</span>
+                Размер <span>29</span>
               </p>
             </div>
             <div className={classes.fourth}>
@@ -81,95 +71,3 @@ const ContentSearch = ({
 };
 
 export default ContentSearch;
-
-// import React from "react";
-//
-// import { useNavigate } from "react-router-dom";
-//
-// import {
-//   blackStarIcon,
-//   colorsPalletIcon,
-//   favoriteIcon,
-// } from "../../../assets/icons/icons";
-// import ProductPhoto from "../../../components/productPhoto/ProductPhoto";
-//
-// import classes from "../search.module.scss";
-//
-// const data = {
-//   price: {
-//     oldPrice: 7500,
-//     newPrice: 5990,
-//   },
-//   colors: 5,
-//   name: "Envy Look All Season Skirt",
-//   sizes: [29, 49],
-//   stars: 4,
-//   isFavorite: false,
-//   // image: bestSellers1,
-//   // id: 1,
-// };
-//
-// interface IProductCard {
-//   image: string;
-//   id: number;
-//   width?: string;
-// }
-//
-// const ContentSearch = ({ image, id, width }: IProductCard) => {
-//   const navigate = useNavigate();
-//
-//   const handleClick = () => {
-//     navigate("/category/4");
-//   };
-//
-//   console.log(data);
-//
-//   return (
-//     <>
-//       <div className={classes.content}>
-//         {/*{data.map((item) => (*/}
-//         {/*  <div>{item.name}</div>*/}
-//         {/*))}*/}
-//         <div className={classes.container}>
-//           <div>
-//             <ProductPhoto image={image} id={id} />
-//           </div>
-//           <div onClick={handleClick} className={classes.content}>
-//             <div className={classes.first}>
-//               <div className={classes.price}>
-//                 <span className={classes.old_price}>{data.price.oldPrice}</span>
-//                 <span className={classes.new_price}>{data.price.newPrice}</span>
-//               </div>
-//               <div className={classes.colors_container}>
-//                 <img src={colorsPalletIcon} alt={"color pallet icon"} />
-//                 <span className={classes.colors}>{data.colors}</span>
-//               </div>
-//             </div>
-//             <div className={classes.second}>
-//               <h4>{data.name}</h4>
-//             </div>
-//             <div className={classes.third}>
-//               <p>
-//                 Размер <span>{data.sizes[0]}</span>
-//               </p>
-//             </div>
-//             <div className={classes.fourth}>
-//               <div className={classes.stars}>
-//                 <img src={blackStarIcon} alt={"star"} />
-//                 <img src={blackStarIcon} alt={"star"} />
-//                 <img src={blackStarIcon} alt={"star"} />
-//                 <img src={blackStarIcon} alt={"star"} />
-//                 <img src={blackStarIcon} alt={"star"} />
-//               </div>
-//               <div className={classes.favorite_icon}>
-//                 <img src={favoriteIcon} alt={"favorite icon"} />
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-//
-// export default ContentSearch;
